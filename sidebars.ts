64c11d5d7504b785de6cf2cc692be5ea5a5f477c
Atulpriya Sharma <sharma.atulpriya@gmail.com>
2024-07-04 10:43:24 +0530

    Update sidebars.ts

--- sidebars.ts
+++ sidebars.ts
@@ -6,7 +6,7 @@ export default {
     { type: "doc", id: "home" },
     {
       type: 'doc',
-      id: 'introduction/index', // document ID
+      id: 'introduction/indexing', // document ID
       label: 'Introduction', // sidebar label
     },
     {
